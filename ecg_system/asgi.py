4f0b6eed3f7818425493ae61df0e3e7109fd36a9
chien24 <phancongchien8278@gmail.com>
2026-04-23 11:19:21 +0700

    fix render realtime: add protocol

--- ecg_system/asgi.py
+++ ecg_system/asgi.py
@@ -11,10 +11,15 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecg_system.settings')
 # Phải gọi get_asgi_application() TRƯỚC khi import Channels routing
 django_asgi_app = get_asgi_application()
 
-from channels.routing import ProtocolTypeRouter, URLRouter  # noqa: E402
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
 from ecg.routing import websocket_urlpatterns  # noqa: E402
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
-    "websocket": URLRouter(websocket_urlpatterns),
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(websocket_urlpatterns)
+        )
+    ),
 })
